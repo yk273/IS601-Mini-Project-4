37746a1134ab9db0f868702dcc109f997246fada
Yana Kokit <yk273@njit.edu>
2019-12-19 04:36:14 -0500

    Feature: add to app file

--- src/app.py
+++ src/app.py
@@ -1,28 +1,28 @@
-# src/app.py
-
 from flask import Flask
 
 from .config import app_config
-from .models import db, bcrypt  # add this new line
+from .models import db, bcrypt
+#from .views.UserView import user_api as user_blueprint
 
 
 def create_app(env_name):
-    """
-    Create app
-    """
+    # app init
 
-    # app initiliazation
     app = Flask(__name__)
 
     app.config.from_object(app_config[env_name])
 
-    # initializing bcrypt
-    bcrypt.init_app(app)  # add this line
+    bcrypt.init_app(app)
 
-    db.init_app(app)  # add this line
+    db.init_app(app)
 
-    #####################
-    # existing code remain #
-    ######################
+  #python run.py  app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+
+    @app.route('/', methods=['GET'])
+    def index():
+        """
+        test endpoint
+        """
+        return 'Congratulations! Your First endpoint is working'
 
     return app
