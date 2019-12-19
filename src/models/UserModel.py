73b3e9a816a30f8792b7dcde3ab3b4f7ccdf4395
Yana Kokit <yk273@njit.edu>
2019-12-19 04:27:02 -0500

    Feature: refactored and reworked UserModel file

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -3,7 +3,6 @@ from marshmallow import fields, Schema
 import datetime
 from . import db
 
-
 class UserModel(db.Model):
     """
     User Model
@@ -54,3 +53,76 @@ class UserModel(db.Model):
 
     def __repr(self):
         return '<id {}>'.format(self.id)
+
+    # src/models/UserModel.py
+    #####################
+    # existing code remain #
+    ######################
+    from ..app import bcrypt  # add this line
+
+    class UserModel(db.Model):
+        """
+        User Model
+        """
+
+        # table name
+        __tablename__ = 'users'
+
+        id = db.Column(db.Integer, primary_key=True)
+        blogposts = db.relationship('BlogpostModel', backref='users', lazy=True)
+
+        #####################
+        # existing code remain #
+        ######################
+
+        # class constructor
+        def __init__(self, data):
+            """
+            Class constructor
+            """
+            self.name = data.get('name')
+            self.email = data.get('email')
+            self.password = self.__generate_hash(data.get('password'))  # add this line
+
+        #####################
+        # existing code remain #
+        ######################
+
+        def update(self, data):
+            for key, item in data.items():
+                if key == 'password':  # add this new line
+                    self.password = self.__generate_hash(value)  # add this new line
+                setattr(self, key, item)
+            self.modified_at = datetime.datetime.utcnow()
+            db.session.commit()
+
+        #####################
+        # existing code remain #
+        ######################
+
+        # add this new method
+        def __generate_hash(self, password):
+            return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
+
+        # add this new method
+        def check_hash(self, password):
+            return bcrypt.check_password_hash(self.password, password)
+
+        #####################
+        # existing code remain #
+        ######################
+
+
+
+
+class UserSchema(Schema):
+    """
+    User Schema
+    """
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
+    blogposts = fields.Nested(BlogpostSchema, many=True)
